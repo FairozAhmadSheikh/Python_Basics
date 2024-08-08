714a6b0f7c7427c68c7f1bb9c808045a14051676
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-08-08 19:19:47 +0530

    Operations on Strings

--- Lists_Tuples.py
+++ Lists_Tuples.py
@@ -7,12 +7,15 @@ print(int(b),type(b))
 #Lists are Mutable but strings are not 
 data =[1,2,3,4,6,8,9,0]
 string="Python"
+
 # Type checking
 print(type(data))
 print(type(string))
 
 #Slicing can be done on both the strings as well as Lists
 #Negative indexing is allowed in both strings as well as Lists
+#If we forget any stating or ending index it will by default be 0 and len(str)
+
 print(string[1:3])
 print(data[2:4])
 print(string[-5:-2])
