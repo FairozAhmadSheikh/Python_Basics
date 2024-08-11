bd9bb43563bc694d042a2f2f82c73d04fd417a08
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-08-11 18:17:20 +0530

    Loops_Ongoing

---
+++ Loops.py
@@ -0,0 +1,9 @@
+# Loops are used for repetation of code or anything that needs to be run for several times
+# Loops are used to repeat instructions
+# print something many times
+
+# While and For Loop
+num=10;
+while num>6:
+    print("hello")
+    
